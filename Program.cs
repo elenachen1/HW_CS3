5542730296d1968018b7e69e7c4520ea078a09cc
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-10-18 23:40:09 +0300

    Задачи 3

--- Program.cs
+++ Program.cs
@@ -69,9 +69,20 @@ while ( caseNumber != 0 ){
             break;
 
         case 3:
+        /* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+        3 -> 1, 8, 27
+        5 -> 1, 8, 27, 64, 125
+        */
 
-            
+        Console. WriteLine ("Введите число: ");
+        int n =int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n ; i=i+1){
         
+          Console.Write($"{Math.Pow(i,3)} ");
+        
+        }
+        Console.WriteLine("");
             break;
         case 4:
             break;
